11d6523f411ad5bc67364006db37d5b2c8344e8f
Lucas Gomes <34803716+ScreamingArrow@users.noreply.github.com>
2021-07-21 23:52:07 -0300

    Create Cadastro.js

---
+++ client/src/pages/Cadastro.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import '../App.css';
+import Form from '../components/Form';
+import NavBar from '../components/NavBar';
+
+export const Cadastro = () => {
+    return (
+        <div>
+            <div className='App'><div>
+            <header>
+            <h1>Cadastro</h1>
+            <title>Cadastro</title>
+            </header>
+            </div>
+                <NavBar/>
+                <Form/>
+            </div>
+        </div>
+    )
+}
