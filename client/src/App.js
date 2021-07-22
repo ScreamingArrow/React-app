f70f513cef0c85c771d03603fedaab05f34bb146
Lucas Gomes <34803716+ScreamingArrow@users.noreply.github.com>
2021-07-21 23:46:02 -0300

    Add files via upload

--- client/src/App.js
+++ client/src/App.js
@@ -1,1 +1,23 @@
+import './App.css';
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { Cadastro } from './pages/Cadastro';
+import { Home } from './pages/Home';
+import { Dashboard } from './pages/Dashboard';
 
+function App() {
+
+
+  return (
+    <div className='App'>
+      <Router>
+        <Switch>
+          <Route path='/' exact component={Home}/>
+          <Route path='/cadastro' exact component={Cadastro}/>
+          <Route path='/dashboard' exact component={Dashboard}/>
+        </Switch>
+      </Router>
+    </div>
+  );
+}
+
+export default App;
